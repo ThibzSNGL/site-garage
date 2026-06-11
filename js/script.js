8b8800ae9e85b5d44369d1f601d0f143366b7e47
ThibzSNGL <thibault.sanglier2005@gmail.com>
2026-06-11 12:16:21 +0200

    script js menu burger

--- js/script.js
+++ js/script.js
@@ -1,1 +1,17 @@
-console.log("Site charger avec succès !");
+const burgerBtn = document.getElementById("burgerBtn");
+const navLinks = document.getElementById("navLinks");
+const menuLinks = document.querySelectorAll(".nav-links a");
+
+burgerBtn.addEventListener("click", () => {
+    burgerBtn.classList.toggle("active");
+    navLinks.classList.toggle("active");
+});
+
+menuLinks.forEach((link) => {
+    link.addEventListener("click", () => {
+        burgerBtn.classList.remove("active");
+        navLinks.classList.remove("active");
+    });
+});
+
+console.log("Site Garage chargé avec succès.");
